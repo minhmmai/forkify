411821bd9b6d20a9d53ac189968c6477f83a2f44
Minh Mai <minhmanmai90@gmail.com>
2019-12-06 16:02:59 +1100

    complete search function, update DOM elements, search view functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
-import axios from 'axios';
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 /**Global state of the app
  * - Search object
@@ -11,23 +12,25 @@ const state = {};
 
 const controlSearch = async () => {
     // 1. Get query from the view
-    const query = 'pizza'; //TODO
+    const query = searchView.getInput(); //TODO
 
     if (query) {
         // 2. New search object and add to state
         state.search = new Search(query);
 
         // 3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResult();
 
         // 4. Search for recipes
-       await state.search.getResults()
+       await state.search.getResults();
 
         // 5. Render results on UI
-        console.log(state.search.result);
+        searchView.renderResults(state.search.results);
     }
 }
 
-document.querySelector('.search').addEventListener('click', e => {
+elements.searchForm.addEventListener('click', e => {
     e.preventDefault();
     controlSearch();
 });
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -7,7 +7,7 @@ export default class Search {
     async getResults() {
         try {
             const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
-            this.result = res.data.recipes;
+            this.results = res.data.recipes;
         } catch{
             alert(error);
         }
